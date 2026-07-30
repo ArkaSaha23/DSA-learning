d99079ad97a4fc186cc363a764f9a8eeb1645e87
Arka Saha <arkacomputer2004@gmail.com>
2026-07-30 20:33:37 +0530

    Create 912. Sort an Array.java

---
+++ Recursion and Backtracking/912. Sort an Array.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int[] sortArray(int[] nums) {
+        mergeSort(nums, 0, nums.length - 1);
+        return nums;
+    }
+
+    public void mergeSort(int[] nums, int left, int right) {
+        if (left >= right){
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+        merge(nums, left, mid, right);
+    }
+
+    public void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;      
+        int j = mid + 1;   
+        int c = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[c++] = nums[i++];
+            } else {
+                temp[c++] = nums[j++];
+            }
+        }
+        while (i <= mid){
+            temp[c++] = nums[i++];
+        }
+        while (j <= right){
+            temp[c++] = nums[j++];
+        }
+        for (int x = 0; x < temp.length; x++) {
+            nums[left + x] = temp[x];
+        }
+    }
+}
